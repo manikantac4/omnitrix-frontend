f4af42fd26fccd39cc4a60150f7a68078809092c
Pandu <panduranga1797@gmail.com>
2025-09-21 22:34:51 +0530

    avanh

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
 // App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Hero from "./components/herosection"
 import GlobalBackground from "./components/globalbackground";
-import Form  from "./components/Register";
+import Form from "./components/Register";
 
-const App = () => {
+// ScrollToTop component - must be inside Router
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Scroll to top on route change and page refresh
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [location.pathname]);
+
+  return null;
+}
+
+function App() {
+  // Additional scroll to top on initial app load
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const HomePage = () => (
+    <>
+      <Hero />
+    </>
+  );
+
   return (
     <Router>
+      <ScrollToTop />
       <GlobalBackground />
-      <div className="relative z-10">
+      <div className="App">
         <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/form" element={<Form/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<Hero />} />
+          <Route path="/form" element={<Form />} />
         </Routes>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
