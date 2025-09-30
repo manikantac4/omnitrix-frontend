2107221335a16cf794268b946e6d6f0969740af9
Panduranga <panduranga1797@gmail.com>
2025-09-30 09:47:46 +0530

    avanh

--- src/components/eventtimeline.jsx
+++ src/components/eventtimeline.jsx
@@ -95,7 +95,7 @@ const Timeline = () => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
       ),
-      date: "7 Oct, 11:59 PM",
+      date: "9 Oct, 11:59 PM",
       title: "Registration Closes"
     },
    
