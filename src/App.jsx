a2be86970297c2006390dada7650db9ce1df6b8c
Panduranga <panduranga1797@gmail.com>
2025-10-03 21:21:39 +0530

    hwik

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import Sponsor from "./components/sponser";
 import Contact from "./components/contact";
 import Prize from "./components/prizes";
 import Hacktime from "./components/hacktime";
-import Timer from "./timer";
-import LoginComponent from "./login";
+//import Timer from "./timer";
+//import LoginComponent from "./login";
 // Import other components as needed
 
 // ScrollToTop component - must be inside Router
