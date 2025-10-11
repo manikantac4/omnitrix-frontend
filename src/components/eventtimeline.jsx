270afbf1ca599eb7d1aca163c1d1a128e8be2b85
Panduranga <panduranga1797@gmail.com>
2025-10-11 09:45:51 +0530

    diqfe

--- src/components/eventtimeline.jsx
+++ src/components/eventtimeline.jsx
@@ -112,7 +112,7 @@ const Timeline = () => {
     {
       icon: (
         <svg className="w-4 h-4 sm:w-5 sm:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
         </svg>
       ),
       date: "10 Oct 7:00 PM",
